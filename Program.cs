446ee7e769d1caccf063488afd326cec0dc5ef09
morganssch <coltm576@gmail.com>
2026-02-19 10:29:44 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Variables_Morgan_Joseph
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace Variables_Morgan_Joseph
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favoriteNumber = 7; //delcare favorite number
+            bool isJumping = false; //making a bool
+            bool isRunnig= false;  //making the other bool
+            float jumpForce = 2f; //declaring the speed
+            double finalGrade = 98.6; //saying final grade
+
+            //Printing everything
+            Console.WriteLine($"My favorite number is {favoriteNumber}");
+            Console.WriteLine($"Is the character jumping? {isJumping}");
+            Console.WriteLine($"Is the character running? {isRunnig}");
+            Console.WriteLine($"Current jump force: {jumpForce}");
+            Console.WriteLine($"Final grade: {finalGrade}");
+
         }
     }
 }
