8b1fc5e64e7c49f4e9b89196e30c416c046d446d
morganssch <coltm576@gmail.com>
2026-02-19 09:18:28 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Variables_Morgan_Joseph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
