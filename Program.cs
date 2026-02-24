01925231f0211b40ade3013f8799f73ed336517e
morganssch <coltm576@gmail.com>
2026-02-24 08:43:55 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,16 +6,17 @@ namespace Variables_Morgan_Joseph
     {
         static void Main(string[] args)
         {
-            int favoriteNumber = 7; //delcare favorite number
-            bool isJumping = false; //making a bool
-            bool isRunnig= false;  //making the other bool
-            float jumpForce = 2f; //declaring the speed
-            double finalGrade = 98.6; //saying final grade
+            int favoriteNumber; //mkaing an int called favorite number
+            bool isJumping = false, isRunning = false;  //making the booleans
+            float jumpForce; //making a float called jumpForce
+            favoriteNumber = 67; //assigning a value to my favorite number
+            jumpForce = 2f; //putting a value on the float
+            const double finalGrade = 98.6; //saying final grade
 
             //Printing everything
             Console.WriteLine($"My favorite number is {favoriteNumber}");
             Console.WriteLine($"Is the character jumping? {isJumping}");
-            Console.WriteLine($"Is the character running? {isRunnig}");
+            Console.WriteLine($"Is the character running? {isRunning}");
             Console.WriteLine($"Current jump force: {jumpForce}");
             Console.WriteLine($"Final grade: {finalGrade}");
 
